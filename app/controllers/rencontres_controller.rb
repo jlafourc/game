6fb9ca1126db35c96504227c5cb04f84e03c0ad3
Julien Lafourcade <julien.lafourcade@gmail.com>
2012-10-16 23:54:59 +0200

    Correction syntaxe rencontre controller

--- app/controllers/rencontres_controller.rb
+++ app/controllers/rencontres_controller.rb
@@ -116,7 +116,7 @@ class RencontresController < ApplicationController
       format.pdf do
         render :pdf => "calendrier.pdf",
                :orientation => 'Landscape',
-               :page_size => 'A4',
+               :page_size => 'A4'
       end
       format.xlsx {
         #fichier = "Calendrier_" + I18n.l(@mois, :format => "%B_%Y") + "_edite_le_" + I18n.l(Time.now, :format => "%d_%m_%Y_a_%H_%M_%S") + ".xlsx"
