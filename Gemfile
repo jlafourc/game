f7b2c19113562e9de0150f40630a28487060474e
Julien Lafourcade <julien.lafourcade@gmail.com>
2012-10-16 23:30:37 +0200

    Essai de wicked-binary en gem

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'mysql2'
 gem 'devise'
 gem 'axlsx_rails'
 gem 'wicked_pdf'
+gem "wkhtmltopdf-binary"
 
 # gem "axlsx", "~> 1.3.1"
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/initializers/wicked_pdf.rb', 'log']
+set :shared_paths, ['config/database.yml', 'log']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
